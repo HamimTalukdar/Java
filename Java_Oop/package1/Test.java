da290cdc267e6b96266202c1c8a71f20945098dd
Hamim <t.i.m.hamim@gmail.com>
2024-02-01 01:05:48 +0600

    Java_Oop project added

---
+++ Java_Oop/package1/Test.java
@@ -0,0 +1,11 @@
+package package1;
+    
+public class Test {
+    int num;
+
+    public static void main(String[] args) {
+        Test ob = new Test();
+        ob.num=3;
+        System.out.println(ob.num);
+    }
+}
